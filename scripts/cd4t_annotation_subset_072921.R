28c6493f709c7d8ba98248510c349401016ea2f1
sphilmoon <sphilmoon@gmail.com>
2021-08-03 12:26:16 +0900

    added and differentiated cd4 subsets with specific genes.

--- scripts/cd4t_annotation_subset_072921.R
+++ scripts/cd4t_annotation_subset_072921.R
@@ -1,28 +1,20 @@
-library(monocle3)
-library(SoupX)
 library(ggplot2)
-library(cowplot)
 library(SingleR)
 library(celldex)
+library(SingleCellExperiment)
 library(Seurat)
-library(SummarizedExperiment)
 library(scran)
 library(patchwork)
-library(EnhancedVolcano)
 library(clusterProfiler)
 library(org.Mm.eg.db)
-library(AnnotationHub)
 library(dplyr)
-library(enrichplot)
-library(SingleCellExperiment)
 
-#Load annotated rds file
-pbmc <- readRDS(file = "/home/Desktop/annotation_working.rds")
-
+# Load annotated rds file
+annotation_integrated <- readRDS(file = "/home/bmrc/Desktop/annotation_working.rds")
 
 # 1st, filter: Subset T cells -----------------------------------------------
 # Can I create a Seurat object of just the Plasma cells and B cells?
-T_cells_1st <- subset((pbmc), idents = c("T cells", "Tgd"))
+T_cells_1st <- subset((annotation_integrated), idents = "T cells")
 
 # How many cells are in each cluster
 table(Idents(T_cells_1st))
@@ -35,7 +27,7 @@ Idents(T_cells_1st) <- "seurat_clusters"
 
 #Identify the B cell sub-clusters at a finer level using SingleR
 results <- SingleR(test = as.SingleCellExperiment(T_cells_1st), ref = ref, assay.type.test=1,
-                   labels = ref$label.fine)
+		                       labels = ref$label.fine)
 results
 
 # Copy over the labels and pruned.labels (Note: any other column of the results could be used as well)
@@ -59,18 +51,16 @@ table(Idents(T_cells_1st))
 
 head(n=400, T_cells_1st)
 
-# save t-cell annotated file
-
 # Type all the fine labels manually
 # Is there other way to list these columns???
 T_cell_subset <- subset((T_cells_1st), idents = c("T cells (T.4MEM49D+11A+.D30.LCMV)", "T cells (T.CD4TESTCJ)", 
-                                                  "T cells (T.4EFF49D+11A+.D8.LCMV)", "T cells (T.4MEM44H62L)", "T cells (T.4MEM)",
-                                                  "T cells (T.CD4.96H)", "T cells (T.CD4.48H)", 
-                                                  "T cells (T.CD4TESTJS)", "T cells (T.4Mem)", "T cells (T.4MEM49D+11A+.D30.LCMV)",
-                                                  "T cells (T.CD4.5H)", "T cells (T.4NVE44-49D-11A-)", "T cells (T.4SP24-)",
-                                                  "T cells (T.CD4.24H)", "T cells (T.4Nve)",
-                                                  "T cells (T.CD4.CTR)", "T cells (T.4.PLN)", "T cells (T.4FP3+25+)"))
-                        
+						                                                    "T cells (T.4EFF49D+11A+.D8.LCMV)", "T cells (T.4MEM44H62L)", "T cells (T.4MEM)",
+												                                                      "T cells (T.CD4.96H)", "T cells (T.CD4.48H)", 
+												                                                      "T cells (T.CD4TESTJS)", "T cells (T.4Mem)", "T cells (T.4MEM49D+11A+.D30.LCMV)",
+																		                                                        "T cells (T.CD4.5H)", "T cells (T.4NVE44-49D-11A-)", "T cells (T.4SP24-)",
+																		                                                        "T cells (T.CD4.24H)", "T cells (T.4Nve)",
+																									                                                  "T cells (T.CD4.CTR)", "T cells (T.4.PLN)", "T cells (T.4FP3+25+)"))
+
 # How many cells are in each cluster
 T_cell_counts <- table(Idents(T_cell_subset), T_cell_subset$group)
 head(T_cell_counts)
@@ -83,16 +73,16 @@ rownames(data_long) <- data_long$x
 data_long
 
 ggp <- ggplot(data_long,            # Create ggplot2 plot scaled to 1.00
-              aes(x = Group,
-                  y = Relative_abundance,
-                  fill = T_cell)) +
+	                    aes(x = Group,
+				                  y = Relative_abundance,
+						                    fill = T_cell)) +
   geom_bar(position = "fill", stat = "identity", color ="black") + 
-  scale_y_continuous(labels = scales::percent_format()) + 
-  scale_x_discrete(name ="Group") +
-  theme(legend.position="right")
+    scale_y_continuous(labels = scales::percent_format()) + 
+      scale_x_discrete(name ="Group") +
+        theme(legend.position="right")
 
 ggp                                 # Draw ggplot2 plot scaled to 1.00
-                        
+
 #Save counts in CSV to see my subset annotation
 #write.csv(T_cell_counts, file = "/home/Public/cd4_subset_annotation")
 
@@ -103,25 +93,60 @@ DimPlot(T_cell_subset, reduction = "umap", split.by = "orig.ident", label = TRUE
 DimPlot(T_cell_subset, reduction = "umap", group.by = "orig.ident", label = TRUE, pt.size = 0.5)
 DimPlot(T_cell_subset, reduction = "umap", group.by = "SingleR.calls_B", label = TRUE, pt.size = 0.5)
 
-
 # find markers for every cluster compared to all remaining cells, report only the positive ones
 T_cell_markers <- FindAllMarkers(T_cell_subset, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T_cell_markers %>%
-  group_by(cluster) %>%
-  top_n(n = 10, wt = avg_log2FC)
+	  group_by(cluster) %>%
+	    top_n(n = 10, wt = avg_log2FC)
+    # look at the most gex in my data
+    head(n=100, T_cell_markers)
 
-# list the gex CD4 T-cell markers
-head(n=100, T_cell_markers)
+    # Use literature genes to define major CD4 gene. Top 15.
+    # drawing a dot plot
+    cd_genes <- c("Cd4", "Cd28", "Ifng", "Tnf", "Foxp3", "Ahr", "Ccr7", "Icos", "Il21", "Lag3", "Lrrc32", "Ccr5")
+    DotPlot(object = T_cell_subset, features = cd_genes) + RotatedAxis() + scale_colour_gradient2(low = "blue", mid = "white", high = "red")
 
-# add dot plot here
-# Use literature genes to define major CD4 gene. Top 15.
-cd_genes <- c("Cd4", "Ifng", "Tnf", "Foxp3", "Ahr", "Ccr7", "Ccr3", "Ccr5", "Ccr6", "Cxcr3", "Icos", "Il18", "Il21", "Lag3", "Lrrc32")
-DotPlot(object = T_cell_subset, features = cd_genes) + RotatedAxis() + scale_colour_gradient2(low = "blue", mid = "white", high = "red")
+    # T.4.PLN T cells genes
+    VlnPlot(T_cell_subset, features = c("Cd4", "Cd28", "Ifng", "Tnf", "Foxp3", "Ahr", "Ccr7", "Icos", "Il21", "Lag3", "Lrrc32", "Ccr5"), pt.size = 0.3, group.by = "orig.ident")  
+    FeaturePlot(T_cell_subset, features = c("Cd4", "Cd28", "Ifng", "Tnf", "Foxp3", "Ahr", "Ccr7", "Icos", "Il21", "Lag3", "Lrrc32", "Ccr5"), pt.size = 0.3) & 
+	    scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
 
-# T.4.PLN T cells genes
-VlnPlot(T_cell_subset, features = c("Cd4", "Ifng", "Tnf", "Foxp3", "Ahr", "Ccr7", "Ccr3"), pt.size = 0.3, group.by = "orig.ident")  
-FeaturePlot(T_cell_subset, features = c("Cd4", "Ifng", "Tnf", "Foxp3", "Ahr", "Ccr7", "Ccr3"), pt.size = 0.3) & 
-  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "RdYlBu")))
+    # look at the most gex in my data, then compare to the literature
+    # Next, plot the most expressed genes in the CD4 subset.
+    # Naive
+    VlnPlot(T_cell_subset, features = c("Cd4", "Cd28", "Ccr7"), pt.size = 0.3, group.by = "orig.ident")
+    FeaturePlot(T_cell_subset, features = c("Cd4", "Cd28", "Ccr7"), pt.size = 0.3)
 
-# look at the most gex in my data, then compare to the literature
-# Next, plot the most expressed genes in the CD4 subset.
+    # Th17
+    VlnPlot(T_cell_subset, features = c("Il21", "Ifng"), pt.size = 0.3, group.by = "orig.ident")
+    FeaturePlot(T_cell_subset, features = c("Il21", "Ifng"), pt.size = 0.3)
+
+    # Tmem
+    VlnPlot(T_cell_subset, features = c("Ccr7", "Il21", "Icos"), pt.size = 0.3, group.by = "orig.ident")
+    FeaturePlot(T_cell_subset, features = c("Ccr7", "Il21", "Icos"), pt.size = 0.3)
+
+    # Treg
+    VlnPlot(T_cell_subset, features = c("Foxp3", "Ahr", "Lrrc32"), pt.size = 0.3, group.by = "orig.ident")
+    FeaturePlot(T_cell_subset, features = c("Foxp3", "Ahr", "Lrrc32"), pt.size = 0.3)
+
+
+    # DIFFERENTIAL GENE EXPRESSION -----------------------------------------------------------------
+    cd4_gex_073121 <- FindAllMarkers(T_cell_subset)
+    head(cd4_gex_073121)
+    write.csv(cd4_gex_073121, file= "/home/bmrc/Desktop/cd4_gex_073121.csv")
+
+    EnhancedVolcano(cd4_gex_073121,
+		                    lab = rownames(cd4_gex_073121),
+				                    x = 'avg_log2FC',
+				                    y = 'p_val',
+						                    pCutoff = 10e-4,
+						                    FCcutoff = 1.5,
+								                    xlim = c(-5.5, 5.5),
+								                    pointSize = 2.0,
+										                    labSize = 3.0,
+										                    title = 'Naive vs 14dpi',
+												                    subtitle = NULL,
+												                    caption = 'FC cutoff, 1.5; p-value cutoff, 10e-4',
+														                    legendLabSize = 14,
+														                    col = c('grey30', 'forestgreen', 'royalblue', 'red2'),
+																                    colAlpha = 0.5,)
